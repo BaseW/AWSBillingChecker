a4fb51e0e9e3a445ce280368b4496b25d1a1a515
wada <wada@avita.co.jp>
2022-06-01 09:11:48 +0900

    fetch pricing rules handler as example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use aws_config::meta::region::RegionProviderChain;
 use aws_lambda_events::event::cloudwatch_logs::{CloudwatchLogsEvent, CloudwatchLogsRawData};
+use aws_sdk_billingconductor::Client;
 use lambda_runtime::{self, run, service_fn, Error, LambdaEvent};
 
 /// This is the main body for the function.
@@ -8,6 +10,20 @@ use lambda_runtime::{self, run, service_fn, Error, LambdaEvent};
 /// - https://github.com/aws-samples/serverless-rust-demo/
 async fn function_handler(event: LambdaEvent<CloudwatchLogsEvent>) -> Result<(), Error> {
     // Extract some useful information from the request
+
+    Ok(())
+}
+
+async fn pricing_rules_handler(event: LambdaEvent<CloudwatchLogsEvent>) -> Result<(), Error> {
+    let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
+    let config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&config);
+    let list_pricing_rules = client.list_pricing_rules();
+    let pricing_rules_result = list_pricing_rules.send().await;
+    match pricing_rules_result {
+        Ok(pricing_rules) => println!("pricing_rules: {:?}", pricing_rules),
+        Err(err) => println!("err: {:?}", err),
+    };
 
     Ok(())
 }
@@ -36,7 +52,8 @@ async fn test_my_lambda_handler() {
 
     let event = lambda_runtime::LambdaEvent::new(input, context);
 
-    function_handler(event)
-        .await
-        .expect("failed to handle event");
+    // function_handler(event)
+    //     .await
+    //     .expect("failed to handle event");
+    pricing_rules_handler(event).await.expect("failed to fetch");
 }
